218bc684f5706377f932b104974713504217c906
gjdev <gjdev@the.src.zone>
2017-06-03 22:53:55 +0200

    fix bundling

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,15 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+//import resolve from 'rollup-plugin-node-resolve';
+//import commonjs from 'rollup-plugin-commonjs';
+import includePaths from 'rollup-plugin-includepaths';
 
 export default {
     entry: 'build/material.js',
     plugins: [
-        resolve({jsnext: true, module: true, main: true, browser: true}),
-        commonjs({
-            include: 'node_modules/rxjs/**'
-        })
+        //resolve({jsnext: true, module: true, main: true, browser: true, exclude: 'node_modules/**'}),
+        //commonjs({
+        //    include: 'node_modules/rxjs/**'
+        //}),
+        includePaths({})
     ],
     onwarn: function(warning) {
         // Suppress known error message caused by TypeScript compiled code with Rollup
@@ -16,7 +18,20 @@ export default {
             console.log("Rollup warning: ", warning.message);
     },
     targets: [
-        {dest: 'dist/material.umd.js' , format: 'umd', moduleName: '@blox/material'},
+        //{dest: 'dist/material.umd.js' , format: 'umd', moduleName: 'blox.material'},
         {dest: 'dist/material.es5.js', format: 'es'}
+    ],
+    external: [
+        '@angular/common',
+        '@angular/core',
+        '@angular/forms',
+        '@material/animation',
+        '@material/checkbox',
+        '@material/form-field',
+        '@material/radio',
+        '@material/ripple',
+        '@material/snackbar',
+        '@material/textfield',
+        '@material/toolbar'
     ]
 };
