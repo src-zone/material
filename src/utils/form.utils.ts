ba0710412abe853ef0a598507670560c19249285
gjdev <gjdev@the.src.zone>
2017-06-04 16:26:01 +0200

    remove form.utils, should not be part of this @blox bundle

--- src/utils/form.utils.ts
+++
@@ -1,7 +0,0 @@
-import { NgForm } from '@angular/forms';
-
-export function markControlsAsTouched(form: NgForm) {
-    for (let name in form.controls)
-        if (form.controls.hasOwnProperty(name))
-            form.controls[name].markAsTouched();
-}
