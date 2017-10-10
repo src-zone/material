d361ee71aa5c3f103cd2a792f3cd058b0129f2c1
gjdev <gjdev@the.src.zone>
2017-10-10 22:39:49 +0200

    docs(site): improve documentation for mdcButton, include API docs

--- bundle/src/components/button/mdc.button.directive.ts
+++ bundle/src/components/button/mdc.button.directive.ts
@@ -3,6 +3,12 @@ import { MDCRipple } from '@material/ripple';
 import { asBoolean } from '../../utils/value.utils';
 import { AbstractMdcRipple } from '../ripple/abstract.mdc.ripple';
 
+/**
+ * Material design button. Anchors can also be styled as buttons with this directive.
+ * Defaults to a button that is flushed with the surface.
+ * Use the input modifiers to alter the styling, or create your own style
+ * based on the provided sass-mixins.
+ */
 @Directive({
     selector: 'button[mdcButton],a[mdcButton]',
     providers: [{provide: AbstractMdcRipple, useExisting: forwardRef(() => MdcButtonDirective) }]
@@ -30,33 +36,10 @@ export class MdcButtonDirective extends AbstractMdcRipple implements AfterConten
             this._ripple.destroy();
     }
 
-    @HostBinding('class.mdc-button--compact') @Input()
-    get mdcCompact() {
-        return this._compact;
-    }
-
-    set mdcCompact(val: any) {
-        this._compact = asBoolean(val);
-    }
-
-    @HostBinding('class.mdc-button--dense') @Input()
-    get mdcDense() {
-        return this._dense;
-    }
-
-    set mdcDense(val: any) {
-        this._dense = asBoolean(val);
-    }
-
-    @HostBinding('class.mdc-button--stroked') @Input()
-    get mdcStroked() {
-        return this._stroked;
-    }
-
-    set mdcStroked(val: any) {
-        this._stroked = asBoolean(val);
-    }
-
+    /**
+     * When this input is defined and does not have value false, the button will be elevated
+     * upon the surface.
+     */
     @HostBinding('class.mdc-button--raised') @Input()
     get mdcRaised() {
         return this._raised;
@@ -66,11 +49,52 @@ export class MdcButtonDirective extends AbstractMdcRipple implements AfterConten
         this._raised = asBoolean(val);
     }
 
+    /**
+     * When this input is defined and does not have value false, the button will be styled 
+     * flush with the surface and have a visible border.
+     */
+    @HostBinding('class.mdc-button--stroked') @Input()
+    get mdcStroked() {
+        return this._stroked;
+    }
+
+    set mdcStroked(val: any) {
+        this._stroked = asBoolean(val);
+    }
+
+    /**
+     * When this input is defined and does not have value false, the amount of horizontal padding
+     * in the button will be reduced.
+     */
+    @HostBinding('class.mdc-button--compact') @Input()
+    get mdcCompact() {
+        return this._compact;
+    }
+
+    set mdcCompact(val: any) {
+        this._compact = asBoolean(val);
+    }
+
+    /**
+     * When this input is defined and does not have value false, the button text is compressed
+     * to make it slightly smaller.
+     */
+    @HostBinding('class.mdc-button--dense') @Input()
+    get mdcDense() {
+        return this._dense;
+    }
+
+    set mdcDense(val: any) {
+        this._dense = asBoolean(val);
+    }
+
+    /** @docs-private TODO not stable */
     activateInputRipple() {
         if (this._ripple)
             this._ripple.activate();
     }
     
+    /** @docs-private TODO not stable */
     deactivateInputRipple() {
         if (this._ripple)
             this._ripple.deactivate();
