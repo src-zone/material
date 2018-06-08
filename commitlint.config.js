183dcfe863b26f0c042efaed32d7c88f6fe983e6
gjdev <gjdev@the.src.zone>
2018-06-08 10:56:04 +0200

    build: improve commitlint, add debug launch configs for vscode

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,45 +1,22 @@
+const { readdirSync, statSync } = require('fs');
+const { join } = require('path');
+const scopes = readdirSync('bundle/src/components');
+
 module.exports = {
     extends: ['@commitlint/config-lerna-scopes'],
     rules: {
         'body-leading-blank': [1, 'always'],
-        lang: [1, 'always', 'eng'],
         'scope-case': [2, 'always', 'lowerCase'],
         'subject-empty': [2, 'never'],
         'subject-full-stop': [2, 'never', '.'],
         'type-case': [2, 'always', 'lowerCase'],
         'type-empty': [2, 'never'],
-        'scope-enum': ctx => [2, 'always', [
-            'button',
-            'card',
-            'checkbox',
-            'dialog',
-            'drawer',
-            'elevation',
-            'fab',
-            'focus-trap',
-            'form-field',
-            'icon-toggle',
-            'linear-progress',
-            'list',
-            'menu',
-            'radio',
-            'ripple',
-            'select',
-            'slider',
-            'snackbar',
-            'switch',
-            'tabs',
-            'text-field',
-            'toolbar',
-            'utility',
-            'utils'
-        ]],
+        'scope-enum': ctx => [2, 'always', scopes],
         'type-enum': [
             2,
             'always',
             [
                 'build',
-                'chore',
                 'ci',
                 'docs',
                 'feat',
@@ -51,5 +28,5 @@ module.exports = {
                 'test'
             ]
         ]
-    }    
+    }
 };
