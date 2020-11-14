3e059dac6eddc1eb3f36a7b6e705ca84584d4251
gjdev <gjdev@the.src.zone>
2020-11-14 16:58:37 +0100

    docs: update site

--- site/src/app/components/directives.demo/index.ts
+++ site/src/app/components/directives.demo/index.ts
@@ -1,79 +1,73 @@
 import { ButtonDirectivesComponent } from './button.directives.component';
-import { CardDirectivesComponent } from './card.directives.component';
-import { CheckboxDirectivesComponent } from './checkbox.directives.component';
-import { ChipsDirectivesComponent } from './chips.directives.component';
-import { DialogDirectivesComponent } from './dialog.directives.component';
-import { DrawerDirectivesComponent } from './drawer.directives.component';
-import { ElevationDirectivesComponent } from './elevation.directives.component';
-import { FabDirectivesComponent } from './fab.directives.component';
-import { FocusTrapDirectivesComponent } from './focus-trap.directives.component';
-import { IconButtonDirectivesComponent } from './icon-button.directives.component';
-import { IconToggleDirectivesComponent } from './icon-toggle.directives.component';
-import { LinearProgressDirectivesComponent } from './linear-progress.directives.component';
-import { ListDirectivesComponent } from './list.directives.component';
-import { MenuDirectivesComponent } from './menu.directives.component';
-import { RadioDirectivesComponent } from './radio.directives.component';
-import { RippleDirectivesComponent } from './ripple.directives.component';
-import { SelectDirectivesComponent } from './select.directives.component';
-import { SliderDirectivesComponent } from './slider.directives.component';
-import { SnackbarDirectivesComponent } from './snackbar.directives.component';
-import { SwitchDirectivesComponent } from './switch.directives.component';
-import { TabDirectivesComponent } from './tab.directives.component';
-import { TextFieldDirectivesComponent } from './text-field.directives.component';
-import { ToolbarDirectivesComponent } from './toolbar.directives.component';
-import { TopAppBarDirectivesComponent } from './top-app-bar.directives.component';
-import { UtilityDirectivesComponent } from './utility.directives.component';
+// import { CardDirectivesComponent } from './card.directives.component';
+// import { CheckboxDirectivesComponent } from './checkbox.directives.component';
+// import { ChipsDirectivesComponent } from './chips.directives.component';
+// import { DialogDirectivesComponent } from './dialog.directives.component';
+// import { DrawerDirectivesComponent } from './drawer.directives.component';
+// import { ElevationDirectivesComponent } from './elevation.directives.component';
+// import { FabDirectivesComponent } from './fab.directives.component';
+// import { FocusTrapDirectivesComponent } from './focus-trap.directives.component';
+// import { IconButtonDirectivesComponent } from './icon-button.directives.component';
+// import { IconToggleDirectivesComponent } from './icon-toggle.directives.component';
+// import { LinearProgressDirectivesComponent } from './linear-progress.directives.component';
+// import { ListDirectivesComponent } from './list.directives.component';
+// import { MenuDirectivesComponent } from './menu.directives.component';
+// import { RadioDirectivesComponent } from './radio.directives.component';
+// import { RippleDirectivesComponent } from './ripple.directives.component';
+// import { SelectDirectivesComponent } from './select.directives.component';
+// import { SliderDirectivesComponent } from './slider.directives.component';
+// import { SnackbarDirectivesComponent } from './snackbar.directives.component';
+// import { SwitchDirectivesComponent } from './switch.directives.component';
+// import { TextFieldDirectivesComponent } from './text-field.directives.component';
+// import { TopAppBarDirectivesComponent } from './top-app-bar.directives.component';
+// import { UtilityDirectivesComponent } from './utility.directives.component';
 
 export * from './button.directives.component';
-export * from './card.directives.component';
-export * from './checkbox.directives.component';
-export * from './chips.directives.component';
-export * from './dialog.directives.component';
-export * from './drawer.directives.component';
-export * from './elevation.directives.component';
-export * from './fab.directives.component';
-export * from './focus-trap.directives.component';
-export * from './icon-button.directives.component';
-export * from './icon-toggle.directives.component';
-export * from './linear-progress.directives.component';
-export * from './list.directives.component';
-export * from './menu.directives.component';
-export * from './radio.directives.component';
-export * from './ripple.directives.component';
-export * from './select.directives.component';
-export * from './slider.directives.component';
-export * from './snackbar.directives.component';
-export * from './switch.directives.component';
-export * from './tab.directives.component';
-export * from './text-field.directives.component';
-export * from './toolbar.directives.component';
-export * from './top-app-bar.directives.component';
-export * from './utility.directives.component';
+// export * from './card.directives.component';
+// export * from './checkbox.directives.component';
+// export * from './chips.directives.component';
+// export * from './dialog.directives.component';
+// export * from './drawer.directives.component';
+// export * from './elevation.directives.component';
+// export * from './fab.directives.component';
+// export * from './focus-trap.directives.component';
+// export * from './icon-button.directives.component';
+// export * from './icon-toggle.directives.component';
+// export * from './linear-progress.directives.component';
+// export * from './list.directives.component';
+// export * from './menu.directives.component';
+// export * from './radio.directives.component';
+// export * from './ripple.directives.component';
+// export * from './select.directives.component';
+// export * from './slider.directives.component';
+// export * from './snackbar.directives.component';
+// export * from './switch.directives.component';
+// export * from './text-field.directives.component';
+// export * from './top-app-bar.directives.component';
+// export * from './utility.directives.component';
 
 export const MDC_DIRECTIVE_DOC_COMPONENTS = [
     ButtonDirectivesComponent,
-    CardDirectivesComponent,
-    CheckboxDirectivesComponent,
-    ChipsDirectivesComponent,
-    DialogDirectivesComponent,
-    DrawerDirectivesComponent,
-    ElevationDirectivesComponent,
-    FabDirectivesComponent,
-    FocusTrapDirectivesComponent,
-    IconButtonDirectivesComponent,
-    IconToggleDirectivesComponent,
-    LinearProgressDirectivesComponent,
-    ListDirectivesComponent,
-    MenuDirectivesComponent,
-    RadioDirectivesComponent,
-    RippleDirectivesComponent,
-    SelectDirectivesComponent,
-    SliderDirectivesComponent,
-    SnackbarDirectivesComponent,
-    SwitchDirectivesComponent,
-    TabDirectivesComponent,
-    TextFieldDirectivesComponent,
-    ToolbarDirectivesComponent,
-    TopAppBarDirectivesComponent,
-    UtilityDirectivesComponent
+    // CardDirectivesComponent,
+    // CheckboxDirectivesComponent,
+    // ChipsDirectivesComponent,
+    // DialogDirectivesComponent,
+    // DrawerDirectivesComponent,
+    // ElevationDirectivesComponent,
+    // FabDirectivesComponent,
+    // FocusTrapDirectivesComponent,
+    // IconButtonDirectivesComponent,
+    // IconToggleDirectivesComponent,
+    // LinearProgressDirectivesComponent,
+    // ListDirectivesComponent,
+    // MenuDirectivesComponent,
+    // RadioDirectivesComponent,
+    // RippleDirectivesComponent,
+    // SelectDirectivesComponent,
+    // SliderDirectivesComponent,
+    // SnackbarDirectivesComponent,
+    // SwitchDirectivesComponent,
+    // TextFieldDirectivesComponent,
+    // TopAppBarDirectivesComponent,
+    // UtilityDirectivesComponent
 ]
