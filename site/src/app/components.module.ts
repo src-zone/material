cacc26889a8a5777b8d1708558fa0588297324cd
gjdev <gjdev@the.src.zone>
2020-11-20 10:24:36 +0100

    docs: add missing import

--- site/src/app/components.module.ts
+++ site/src/app/components.module.ts
@@ -48,9 +48,7 @@ import {
     ...MDC_DIRECTIVE_DOC_COMPONENTS,
 
     SnippetButtonComponent,
-    // SnippetCardComponent,
-    // SnippetCheckboxComponent,
-    // SnippetChipsComponent, SnippetChipsChoiceComponent, SnippetChipsFilterComponent, SnippetChipsInputComponent,
+    SnippetCardComponent,
     // SnippetDialogComponent,
     // SnippetDrawerPermanentBelowComponent,
     // SnippetDrawerPermanentComponent,
