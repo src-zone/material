86c16889b73a807b4c5e43e19486928aa7d4e0a1
gjdev <gjdev@the.src.zone>
2020-12-04 22:29:27 +0100

    docs: theme switcher palette icon added

--- site/src/app/components/theme/theme.switcher.component.ts
+++ site/src/app/components/theme/theme.switcher.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { Angulartics2 } from 'angulartics2';
 import { first } from 'rxjs/operators';
 import { ThemeService } from '../../services';
@@ -8,12 +9,15 @@ import { ThemeService } from '../../services';
   templateUrl: './theme.switcher.component.html'
 })
 export class ThemeSwitcherComponent {
+    static SVG = require('!raw-loader!../../../assets/img/themes/palette.svg').default;
+    svg: SafeHtml;
     theme: string;
     themes: string[];
 
-    constructor(private themeService: ThemeService, private angulartics2: Angulartics2) {
+    constructor(private themeService: ThemeService, private angulartics2: Angulartics2, private sanitizer: DomSanitizer) {
         this.themes = themeService.themes;
         themeService.theme$.pipe(first()).subscribe(theme => this.theme = theme);
+        this.svg = this.sanitizer.bypassSecurityTrustHtml(ThemeSwitcherComponent.SVG);
     }
 
     getName(theme: string) {
@@ -23,5 +27,12 @@ export class ThemeSwitcherComponent {
     setTheme(theme: string) {
       this.theme = theme;
       this.themeService.theme = theme;
+      this.angulartics2.eventTrack.next({
+        action: 'theme',
+        properties: {
+            category: 'switch',
+            label: theme
+        }
+      });
     }
 }
