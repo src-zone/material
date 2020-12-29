8f44848ecfcd880d2ff52e9ecc0467e0b49fd557
gjdev <gjdev@the.src.zone>
2020-12-29 16:52:57 +0100

    debug

--- bundle/src/components/chips/mdc.chip.directive.spec.ts
+++ bundle/src/components/chips/mdc.chip.directive.spec.ts
@@ -109,7 +109,7 @@ describe('MdcChipDirective', () => {
         expect(chipComponents[2].value).toBe('three');
     }));
 
-    it('chipset type is one of choice, filter, input, or action', (() => {
+    fit('chipset type is one of choice, filter, input, or action', (() => {
         const { fixture, testComponent, chipSetComponent, chipSet } = setup();
         expect(chipSetComponent.mdcChipSet).toBe('action');
         expect(chipSet.getAttribute('class')).toBe('mdc-chip-set');
@@ -202,7 +202,7 @@ describe('MdcChipDirective', () => {
         expect(testComponent.chips).toEqual([]);
     }));
 
-    it('after chip removal, next remaining chip has focus and is tabbable', fakeAsync(() => {
+    fit('after chip removal, next remaining chip has focus and is tabbable', fakeAsync(() => {
         const { fixture, testComponent } = setup();
         testComponent.type = 'input';
         testComponent.chips = ['chip1', 'chip2', 'chip3'];
